655440cf93f9442da0bf981f616ae00ea0af093f
Maya Driver <mayadrivermd@gmail.com>
2014-08-31 20:09:08 +0100

    makers academy week 4 challenge

---
+++ lib/takeaway.rb
@@ -0,0 +1,51 @@
+class Takeaway
+	attr_reader :order
+
+	def initialize
+		@order = []
+	end
+
+	def menu
+		{:green_curry => 5, :red_curry => 6}
+	end
+
+	def ring_ring
+		answer = how_can_i_help_you		
+		what_do_you_want if answer == "2"
+		menu.each { |key, value| p "#{key.to_s.gsub("_", " ")}: £#{value}" } if answer == "1"
+	end
+
+	def how_can_i_help_you
+		puts "How can I help you? Choose 1 to see the menu, 2 to place an order and type \"x\" to stop."
+		gets.chomp
+	end
+
+	def what_next
+		puts "Do you want to place an order? Type \"order\""
+		gets.chomp
+	end
+
+	def what_do_you_want
+		order = gets.chomp
+		until order == "x" do
+			order_to_sym = order.gsub(" ", "_").to_sym
+			if menu.include?(order_to_sym) 
+				@order << order_to_sym
+				order = gets.chomp
+			else
+				p "sowwy, cant hear you?" if !menu.include?(order_to_sym)
+				order = gets.chomp
+			end
+		end
+	end
+
+	def total
+		p order.inspect
+		order.map! do |v|
+			x = v.to_s.gsub(" ", "_").to_sym
+			menu[x]
+		end
+		order.inject(:+)
+	end
+
+end
---
+++ spec/takeaway_spec.rb
@@ -0,0 +1,51 @@
+require 'takeaway'
+
+describe Takeaway do
+
+	let(:takeaway) {Takeaway.new}
+
+	it 'should have a menu of dishes with prices' do
+		expect(takeaway.menu).to eq({:green_curry => 5, :red_curry => 6})
+	end
+
+	it "prints out the menu when prompted" do
+		expect(takeaway).to receive(:how_can_i_help_you).and_return("1")
+		expect(takeaway.ring_ring).to eq({:green_curry => 5, :red_curry => 6})
+	end
+
+	it 'should be able to take an order' do
+		allow(takeaway).to receive(:how_can_i_help_you).and_return("2")
+		expect(takeaway).to receive(:what_do_you_want)
+		takeaway.ring_ring
+	end
+
+	it 'should receive input when asked what do you want' do
+		expect(takeaway).to receive(:gets).and_return("red curry\n")
+		allow(takeaway).to receive(:gets).and_return("x\n")
+		takeaway.what_do_you_want
+	end
+
+	it 'should add dishes to the order' do
+		allow(takeaway).to receive(:gets).and_return("green curry\n")
+		allow(takeaway).to receive(:gets).and_return("x\n")
+		takeaway.what_do_you_want
+		expect(takeaway.order).to eq ([:green_curry])
+	end
+
+	xit 'should continue to ask for dishes until the customer is done' do
+		allow(takeaway).to receive(:order).and_return(["red curry\n"])
+		allow(takeaway).to receive(:order).and_return(["red curry\n"])
+		takeaway.what_do_you_want
+		# allow(takeaway).to receive(:gets).and_return("green curry\n")
+		
+		expect(takeaway.order).to eq {[:red_curry, :red_curry]}
+	end
+
+	it 'should add the prices together' do
+		allow(takeaway).to receive(:order).and_return(["red curry", "green curry"])
+		expect(takeaway.total).to eq 11
+	end
+
+	#cd projects/challenges/week_4_challenge/takeaway
+
+end
