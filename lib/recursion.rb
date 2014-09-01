e42ec12913f0420ed698251c0a189e57a510f794
Maya Driver <mayadrivermd@gmail.com>
2014-09-01 19:29:23 +0100

    Delete recursion.rb

--- lib/recursion.rb
+++
@@ -1,9 +0,0 @@
-def palindrome?(word)
-	return true if word.length <= 1
-	return false if word.slice!(0) != word.slice!((word.length) -1)
-	palindrome?(word)
-end
-
-p palindrome?("maya")
-p palindrome?("racecar")
-p palindrome?("afgfgdfgfda")
