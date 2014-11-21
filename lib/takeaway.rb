3e94d6f0dee9b2b923392208bb94b84844021326
Maya Driver <mayadrivermd@gmail.com>
2014-11-21 21:41:56 +0000

    fixing indentation

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,66 +1,66 @@
 require 'twilio-ruby'
 
 class Takeaway
-	attr_reader :order
+  attr_reader :order
 
-	def initialize
-		@order = []
-	end
+  def initialize
+    @order = []
+  end
 
-	def menu
-		{:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2}
-	end
+  def menu
+    {:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2}
+  end
 
-	def ring_ring
-		p "How can I help you? To see the menu, type 1." 
-		loop do
-			options
-		end
-	end
+  def ring_ring
+    p "How can I help you? To see the menu, type 1."
+    loop do
+      options
+    end
+  end
 
-	def options
-		p "Choose 2 to add items to your order or 3 to pay."
-		answer = gets.chomp
-		case answer
-		when "1"
-			menu.each { |key, value| p "#{key.to_s.gsub("_", " ")}: £#{value}" }
-		when "2"	
-			what_do_you_want
-		when "3"
-			pay
-		else 
-			p 'Try again'
-		end
-	end
+  def options
+    p "Choose 2 to add items to your order or 3 to pay."
+    answer = gets.chomp
+    case answer
+    when "1"
+      menu.each { |key, value| p "#{key.to_s.gsub("_", " ")}: £#{value}" }
+    when "2"
+      what_do_you_want
+    when "3"
+      pay
+    else
+      p 'Try again'
+    end
+  end
 
-	def what_do_you_want
-		place_order = gets.chomp
-		order_to_sym = place_order.gsub(" ", "_").to_sym
-		@order << order_to_sym if menu.include?(order_to_sym) 
-		p "We don't serve that here. Please try again." unless menu.include?(order_to_sym)
-		all_dishes
-	end
+  def what_do_you_want
+    place_order = gets.chomp
+    order_to_sym = place_order.gsub(" ", "_").to_sym
+    @order << order_to_sym if menu.include?(order_to_sym)
+    p "We don't serve that here. Please try again." unless menu.include?(order_to_sym)
+    all_dishes
+  end
 
-	def pay
-		p "You have ordered: #{@dishes}, which comes to £#{total}. We will text to confirm your order."
-		send_message
-		exit
-	end
+  def pay
+    p "You have ordered: #{@dishes}, which comes to £#{total}. We will text to confirm your order."
+    send_message
+    exit
+  end
 
-	def all_dishes
-		@dishes = order.map! {|i| i.to_s}.join(", ").gsub("_", " ")
-	end
+  def all_dishes
+    @dishes = order.map! {|i| i.to_s}.join(", ").gsub("_", " ")
+  end
 
-	def total
-		new_order = order.map {|v| x = v.to_s.gsub(" ", "_").to_sym ; menu[x] }
-		new_order.inject(:+)
-	end
+  def total
+    new_order = order.map {|v| x = v.to_s.gsub(" ", "_").to_sym ; menu[x] }
+    new_order.inject(:+)
+  end
 
-	def send_message
-		@account_sid = ''
-		@auth_token = ''
-		@client = Twilio::REST::Client.new(@account_sid, @auth_token)
-		@client.messages.create(:from => '1234567890', :to => '1234567890', :body => 'Thank you! Your order was placed and will be delivered within one hour.')
-	end
+  def send_message
+    @account_sid = ''
+    @auth_token = ''
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @client.messages.create(:from => '1234567890', :to => '1234567890', :body => 'Thank you! Your order was placed and will be delivered within one hour.')
+  end
 
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,67 +2,67 @@ require 'takeaway'
 
 describe Takeaway do
 
-	before(:each) do
-		allow(takeaway).to receive(:send_message)
-	end
+  before(:each) do
+    allow(takeaway).to receive(:send_message)
+  end
 
-	let(:takeaway) {Takeaway.new}
+  let(:takeaway) {Takeaway.new}
 
-	it 'should have a menu of dishes with prices' do
-		expect(takeaway.menu).to eq({:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2})
-	end
+  it 'should have a menu of dishes with prices' do
+    expect(takeaway.menu).to eq({:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2})
+  end
 
-	it "prints out the menu when prompted" do
-		allow(takeaway).to receive(:gets).and_return("1")
-		takeaway.options
-		expect(takeaway.options).to eq({:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2})
-	end
+  it "prints out the menu when prompted" do
+    allow(takeaway).to receive(:gets).and_return("1")
+    takeaway.options
+    expect(takeaway.options).to eq({:green_curry => 5, :red_curry => 6, :jasmine_rice => 4, :prawn_crackers => 2})
+  end
 
-	it 'should be able to take an order' do
-		allow(takeaway).to receive(:gets).and_return("2")
-		expect(takeaway).to receive(:what_do_you_want)
-		takeaway.options
-	end
+  it 'should be able to take an order' do
+    allow(takeaway).to receive(:gets).and_return("2")
+    expect(takeaway).to receive(:what_do_you_want)
+    takeaway.options
+  end
 
-	it 'should receive input when asked what do you want' do
-		expect(takeaway).to receive(:gets).and_return("red curry\n")
-		takeaway.what_do_you_want
-	end
-		
-	it 'should add dishes to the order' do
-		allow(takeaway).to receive(:gets).and_return("green curry\n")
-		takeaway.what_do_you_want
-		expect(takeaway.order).to eq (["green_curry"])
-	end
+  it 'should receive input when asked what do you want' do
+    expect(takeaway).to receive(:gets).and_return("red curry\n")
+    takeaway.what_do_you_want
+  end
 
-	context 'adding multiple dishes to the order' do
-		
-		before(:each) do
-			allow(takeaway).to receive(:order).and_return(["red curry", "green curry"])
-		end
+  it 'should add dishes to the order' do
+    allow(takeaway).to receive(:gets).and_return("green curry\n")
+    takeaway.what_do_you_want
+    expect(takeaway.order).to eq (["green_curry"])
+  end
 
-		it 'should list all dishes ordered' do
-			expect(takeaway.all_dishes).to eq("red curry, green curry")
-		end
-		
-		it 'should add the prices together' do
-			expect(takeaway.total).to eq 11
-		end
+  context 'adding multiple dishes to the order' do
 
-	end
+    before(:each) do
+      allow(takeaway).to receive(:order).and_return(["red curry", "green curry"])
+    end
 
-		it 'should take payment' do
-			allow(takeaway).to receive(:gets).and_return("3")
-			expect(takeaway).to receive(:pay)
-			takeaway.options
-		end
+    it 'should list all dishes ordered' do
+      expect(takeaway.all_dishes).to eq("red curry, green curry")
+    end
 
-		it 'should send a confirmation text message and exit the program' do
-			begin 
-				takeaway.pay 
-				rescue SystemExit 
-			end
-			expect{takeaway.pay}.to raise_error SystemExit
-		end
+    it 'should add the prices together' do
+      expect(takeaway.total).to eq 11
+    end
+
+  end
+
+    it 'should take payment' do
+      allow(takeaway).to receive(:gets).and_return("3")
+      expect(takeaway).to receive(:pay)
+      takeaway.options
+    end
+
+    it 'should send a confirmation text message and exit the program' do
+      begin
+        takeaway.pay
+        rescue SystemExit
+      end
+      expect{takeaway.pay}.to raise_error SystemExit
+    end
 
 end
